b181de9e776854f1e1bc00d5aad78ef61e900931
97kmj <kmj0376@gmail.com>
2024-09-03 11:13:43 +0900

    test 12345

--- src/main/java/org/example/TestFile.java
+++ src/main/java/org/example/TestFile.java
@@ -3,8 +3,8 @@ package org.example;
 public class TestFile {
 
 
-  //테스트1 ㅇㅇㅇ
-  //테스트1
+  //테스트1 dddddd
+  //테스트1dddd
   //테스트1
 
 }
--- src/main/java/org/example/TestFile2.java
+++ src/main/java/org/example/TestFile2.java
@@ -3,10 +3,9 @@ package org.example;
 public class TestFile2 {
 
 
-
+    // 테스트
 
     //테스트2 ㄷㄷㄷ
-    //테스트2
-    //테스트2
+    //테스트2ddddd
 
   }
