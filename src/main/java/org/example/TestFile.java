1a9c35db3972c752413f1447ee5a8007776128af
SantinoMin <santinomin0381@gmail.com>
2024-09-03 10:51:33 +0900

    테스트 1,2

--- src/main/java/org/example/TestFile.java
+++ src/main/java/org/example/TestFile.java
@@ -3,8 +3,8 @@ package org.example;
 public class TestFile {
 
 
-  //테스트2
-  //테스트2
-  //테스트2
+  //테스트1
+  //테스트1
+  //테스트1
 
 }
---
+++ src/main/java/org/example/TestFile2.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class TestFile2 {
+
+
+
+
+    //테스트2
+    //테스트2
+    //테스트2
+
+  }
