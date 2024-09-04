d0330c0e04ec7b96ada10107b6588c3ffcbe9997
lsenaa <lsenaa3838@gmail.com>
2024-09-04 17:02:09 +0900

    fix: influencer service 수정

--- ProjectFiin/src/main/java/service/InfluencerService.java
+++ ProjectFiin/src/main/java/service/InfluencerService.java
@@ -4,8 +4,8 @@ package service;
 import dto.Influencer;
 
 public interface InfluencerService {
-	void join(Influencer influencer) throws Exception;
-	void login(Integer influencerNum, String userEmail, String password) throws Exception;
-	boolean checkDoubleId(Integer influencerNum, String userEmail) throws Exception;
 	Influencer register(Influencer influencer) throws Exception;
+	void join(Influencer influencer) throws Exception;
+	void login(Integer influencerNum, String password) throws Exception;
+	boolean checkDoubleEmail(String userEmail) throws Exception;
 }
--- ProjectFiin/src/main/java/service/InfluencerServiceImpl.java
+++ ProjectFiin/src/main/java/service/InfluencerServiceImpl.java
@@ -31,22 +31,22 @@ public class InfluencerServiceImpl implements InfluencerService {
 	@Override
 	public void join(Influencer influencer) throws Exception {
 		Influencer sinfluencer = influencerDao.selectInfluencer(influencer.getInfluencerNum());
-		if ((sinfluencer.getUserEmail()).equals(influencer.getUserEmail())) throw new Exception("아이디 중복 오류");
+		if (sinfluencer != null) throw new Exception("아이디 중복 오류");
 		influencerDao.insertInfluencer(influencer);
 	}
 
 	@Override
-	public void login(Integer influencerNum, String userEmail, String password) throws Exception {
+	public void login(Integer influencerNum, String password) throws Exception {
 		Influencer influencer = influencerDao.selectInfluencer(influencerNum);
-		if (influencer.getUserEmail() == null || !userEmail.equals(influencer.getUserEmail())) throw new Exception("로그인 아이디 오류");
+		if (influencer == null) throw new Exception("로그인 아이디 오류");
 		if (!password.equals(influencer.getPassword())) throw new Exception("비밀번호 오류");
 
 	}
 
 	@Override
-	public boolean checkDoubleId(Integer influencerNum, String userEmail) throws Exception {
-		Influencer influencer = influencerDao.selectInfluencer(influencerNum);
-		if (!(influencer.getUserEmail()).equals(userEmail)) return false;
+	public boolean checkDoubleEmail(String userEmail) throws Exception {
+		Influencer influencer = influencerDao.selectInfluencerByEmail(userEmail);
+		if (influencer == null) return false;
 		return true;
 	}
 }
