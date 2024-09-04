5815ec67b44fc94d00e9087099bbb5176d6cf1e7
KOSTA <junyung0721@naver.com>
2024-09-04 19:54:27 +0900

    feat: 캠페인화면 재 수정

--- ProjectFiin/src/main/java/dao/CampaignDao.java
+++ ProjectFiin/src/main/java/dao/CampaignDao.java
@@ -1,12 +1,18 @@
 package dao;
 
 
+import java.util.List;
+
 import dto.Campaign;
 
 public interface CampaignDao {
 
-  void registerCampaign(Campaign
-      campaign) throws Exception;
-
+	void registerCampaign(Campaign campaign) throws Exception;
+	
+	Campaign selectCampaign(Integer campaignNum)throws Exception;
+	
+	Integer selectCampaignCount()throws Exception;
+	
+	List<Campaign> selectCampaignList(Integer row) throws Exception;
 //	Member selectMember(String id) throws Exception;
 }
