f2bd6d2fcef27752f4fb1d3afc52c01024160291
lsenaa <lsenaa3838@gmail.com>
2024-09-04 16:58:35 +0900

    fix: AdvertiserService 수정

--- ProjectFiin/src/main/java/service/AdvertiserService.java
+++ ProjectFiin/src/main/java/service/AdvertiserService.java
@@ -4,6 +4,6 @@ import dto.Advertiser;
 
 public interface AdvertiserService {
 	void join(Advertiser advertiser) throws Exception;
-	void login(Integer advertiserNum, String userEmail, String password) throws Exception;
-	boolean checkDoubleId(Integer advertiserNum, String userEmail) throws Exception;
+	void login(Integer advertiserNum, String password) throws Exception;
+	boolean checkDoubleEmail(Integer advertiserNum) throws Exception;
 }
--- ProjectFiin/src/main/java/service/AdvertiserServiceImpl.java
+++ ProjectFiin/src/main/java/service/AdvertiserServiceImpl.java
@@ -13,21 +13,21 @@ public class AdvertiserServiceImpl implements AdvertiserService {
 	@Override
 	public void join(Advertiser advertiser) throws Exception {
 		Advertiser sadvertiser = advertiserDao.selectAdvertiser(advertiser.getAdvertiserNum());
-		if ((sadvertiser.getUserEmail()).equals(advertiser.getUserEmail())) throw new Exception("아이디 중복 오류");
+		if (sadvertiser != null) throw new Exception("아이디 중복 오류");
 		advertiserDao.insertAdvertiser(advertiser);
 	}
 
 	@Override
-	public void login(Integer advertiserNum, String userEmail, String password) throws Exception {
+	public void login(Integer advertiserNum, String password) throws Exception {
 		Advertiser advertiser = advertiserDao.selectAdvertiser(advertiserNum);
-		if (advertiser.getUserEmail() == null || !userEmail.equals(advertiser.getUserEmail())) throw new Exception("로그인 오류");
+		if (advertiser == null) throw new Exception("로그인 오류");
 		if (!password.equals(advertiser.getPassword())) throw new Exception("비밀번호 오류");
 	}
 
 	@Override
-	public boolean checkDoubleId(Integer advertiserNum, String userEmail) throws Exception {
+	public boolean checkDoubleEmail(Integer advertiserNum) throws Exception {
 		Advertiser advertiser = advertiserDao.selectAdvertiser(advertiserNum);
-		if (!(advertiser.getUserEmail()).equals(userEmail)) return false;
+		if (advertiser == null) return false;
 		return true;
 	}
 }
