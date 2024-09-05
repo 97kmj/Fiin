a082f5385e9343a05e4e48fe92a8d02e393f9fc2
lsenaa <lsenaa3838@gmail.com>
2024-09-05 10:14:17 +0900

    fix: 충돌 수정

--- ProjectFiin/src/main/java/service/CampaignService.java
+++ ProjectFiin/src/main/java/service/CampaignService.java
@@ -1,25 +1,11 @@
 package service;
 
-<<<<<<< HEAD
-
 import java.util.List;
 
 import dto.Campaign;
 import util.PageInfo;
 
 public interface CampaignService {
-
   Campaign register(Campaign campaign) throws Exception;
-  
   List<Campaign> campaignList(PageInfo pageinfo)throws Exception;
-  
-=======
-import java.util.List;
-
-import dto.Campaign;
-import util.PageInfo;
-
-public interface CampaignService {
-	List<Campaign> campaignList(PageInfo pageinfo)throws Exception;
->>>>>>> refs/remotes/origin/develop-jy
 }
