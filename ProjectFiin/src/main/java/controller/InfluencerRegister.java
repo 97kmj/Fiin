95b897789859b925ab4e9fe2640281545778eab3
SantinoMin <santinomin0381@gmail.com>
2024-09-05 12:17:49 +0900

    fix : Influencer register (jsp,service) 부분 수정.

---
+++ ProjectFiin/src/main/java/controller/InfluencerRegister.java
@@ -0,0 +1,73 @@
+package controller;
+
+// 7.1.1 인플루언서 등록
+
+import dto.Influencer;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import service.InfluencerService;
+import service.InfluencerServiceImpl;
+
+@WebServlet("/registerInfluencer")
+public class InfluencerRegister extends HttpServlet {
+
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    super.doGet(req, resp);
+  }
+
+
+  //인플루언서 등록
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    //1. 요청으로부터 데이터 가져오기
+    request.setCharacterEncoding("UTF-8");
+
+    String introLine = request.getParameter("introLine");
+    String profileImage = request.getParameter("profileImage");
+
+    String youtubeName = request.getParameter("youtubeName");
+    Integer youtubeFollower = Integer.parseInt(request.getParameter("youtubeFollower"));
+    String youtubeUrl = request.getParameter("youtubeUrl");
+
+    String instagramName = request.getParameter("instagramName");
+    Integer instagramFollower = Integer.parseInt(request.getParameter("instagramFollower"));
+    String instagramUrl = request.getParameter("instagramUrl");
+
+    String blogName = request.getParameter("blogName");
+    Integer blogFollower = Integer.parseInt(request.getParameter("blogFollower"));
+    String blogUrl = request.getParameter("blogUrl");
+
+    //파라미터로 가져오는 category는 array형식(복수형)
+    Integer categoryId = Integer.parseInt(request.getParameter("category"));
+
+    String introduction = request.getParameter("introduction");
+
+    // Influencer 객체 생성
+    Influencer inf = new Influencer(introLine, profileImage, youtubeName, youtubeFollower,
+        youtubeUrl, instagramName, instagramFollower, instagramUrl,
+        blogName, blogFollower, blogUrl, categoryId, introduction);
+
+    try {
+      //2. 데이터 처리하기 : Model
+      InfluencerService service = new InfluencerServiceImpl();
+      Influencer inff = service.influencerRegister(inf);
+
+      //3. 처리한 데이터 View 지정하여 넘겨주기
+      request.setAttribute("influencer", inff);
+      request.getRequestDispatcher("accountinfo.jsp").forward(request, response);
+      ////////////////////////
+    } catch (Exception e) {
+      e.printStackTrace();
+      request.setAttribute("err", e.getMessage());
+      request.getRequestDispatcher("err.jsp").forward(request, response);
+    }
+  }
+}
--- ProjectFiin/src/main/java/service/InfluencerServiceImpl.java
+++ ProjectFiin/src/main/java/service/InfluencerServiceImpl.java
@@ -1,63 +1,72 @@
 package service;
 
 
-import java.util.List;
-
 import dao.InfluencerDao;
 import dao.InfluencerDaoImpl;
 import dto.Influencer;
+import java.util.List;
 import util.PageInfo;
 
 public class InfluencerServiceImpl implements InfluencerService {
 
-  private InfluencerDao influencerDao;
+	private InfluencerDao influencerDao;
 
-  public InfluencerServiceImpl() {
-    influencerDao = new InfluencerDaoImpl();
-  }
+	public InfluencerServiceImpl() {
+		influencerDao = new InfluencerDaoImpl();
+	}
 
-  @Override
-  public Influencer register(Influencer inf) throws Exception {
+	@Override
+	public Influencer register(Influencer inf) throws Exception {
 
-//    Member smember = memberDao.selectMember(member.getId());
-//    if(smember!=null) throw new Exception("아이디 중복 오류");
-//    memberDao.insertMember(member);
+		influencerDao.registerInfluencer(inf);
+		//dao에 데이터를 넣으면 끝 맞는지?
+		return inf;
 
-//    Influencer inff = influencerDao.registerInfluencer(inf.);
-
-    influencerDao.registerInfluencer(inf);
-    //dao에 데이터를 넣으면 끝 맞는지?
-    return inf;
-
-  }
-
+	}
 
 
 	@Override
 	public void join(Influencer influencer) throws Exception {
 		Influencer sinfluencer = influencerDao.selectInfluencer(influencer.getInfluencerNum());
-		if (sinfluencer != null) throw new Exception("아이디 중복 오류");
+		if (sinfluencer != null)
+			throw new Exception("아이디 중복 오류");
 		influencerDao.insertInfluencer(influencer);
 	}
 
 	@Override
 	public void login(Integer influencerNum, String password) throws Exception {
 		Influencer influencer = influencerDao.selectInfluencer(influencerNum);
-		if (influencer == null) throw new Exception("로그인 아이디 오류");
-		if (!password.equals(influencer.getPassword())) throw new Exception("비밀번호 오류");
+		if (influencer == null)
+			throw new Exception("로그인 아이디 오류");
+		if (!password.equals(influencer.getPassword()))
+			throw new Exception("비밀번호 오류");
 
 	}
 
 	@Override
 	public boolean checkDoubleEmail(String userEmail) throws Exception {
 		Influencer influencer = influencerDao.selectInfluencerByEmail(userEmail);
-		if (influencer == null) return false;
+		if (influencer == null)
+			return false;
 		return true;
 	}
-  @Override
-  public List<Influencer> influencerList(PageInfo pageInfo) {
-	// TODO Auto-generated method stub
-	return null;
+
+	@Override
+	public List<Influencer> influencerList(PageInfo pageInfo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Influencer influencerRegister(Influencer influencer) throws Exception {
+		// Dto에서 받은 정보들을 dao에 전달
+		influencerDao.registerInfluencer(influencer);
+		return influencer;
+	}
 }
 
-}
+
+
+
+
+
