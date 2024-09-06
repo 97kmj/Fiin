4328db4c12b2bc7e15c0ba2c8240518962fcf296
lsenaa <lsenaa3838@gmail.com>
2024-09-06 09:19:01 +0900

    modify: 로그인시 type session에 저장

--- ProjectFiin/src/main/java/controller/Login.java
+++ ProjectFiin/src/main/java/controller/Login.java
@@ -54,31 +54,16 @@ public class Login extends HttpServlet {
 				influencer.setPassword("");
 				request.getSession().removeAttribute("advertiser");
 				request.getSession().setAttribute("influencer", influencer);
+				request.getSession().setAttribute("type", type);
 				response.sendRedirect("main");
-				
-				//System.out.println(influencer);
-				/*
-				 * if (influencer == null) { response.getWriter().write(String.valueOf(false));
-				 * } else { //response.getWriter().write(String.valueOf(true));
-				 * influencer.setPassword(""); request.getSession().setAttribute("influencer",
-				 * influencer); //request.getRequestDispatcher("main").forward(request,
-				 * response); response.sendRedirect("main"); }
-				 */
 			} else {
 				Advertiser advertiser = aService.login(userEmail, password);
 				advertiser.setPassword("");
 				request.getSession().removeAttribute("influencer");
+				request.getSession().setAttribute("type", type);
 				request.getSession().setAttribute("advertiser", advertiser);
 				response.sendRedirect("main");
-						
-				//System.out.println(advertiser);
-				/*
-				 * if (advertiser == null) { response.getWriter().write(String.valueOf(false));
-				 * } else { //response.getWriter().write(String.valueOf(true));
-				 * advertiser.setPassword(""); request.getSession().setAttribute("advertiser",
-				 * advertiser); //request.getRequestDispatcher("main").forward(request,
-				 * response); response.sendRedirect("main"); }
-				 */
+					
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
