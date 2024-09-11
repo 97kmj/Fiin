17c143d56388b2c1bbd7f00a79e1f691f88a74a8
97kmj <kmj0376@gmail.com>
2024-09-11 18:13:29 +0900

    feat:mailutil 메일 양식 수정중

--- ProjectFiin/src/main/java/util/MailUtil.java
+++ ProjectFiin/src/main/java/util/MailUtil.java
@@ -67,7 +67,6 @@ public class MailUtil {
 					+ "    </tr>\r\n"
 					+ "    <tr>\r\n"
 					+ "        <td style=\"padding-left: 20px;\">"+campaign.getCampaignTitle()+"</td>\r\n"
-					
 					+ "    </tr>\r\n"
 					+ "\r\n"
 					+ "    <tr>\r\n"
