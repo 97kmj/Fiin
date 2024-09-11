d551785d2870f761351c7d0c612a99632d2b447e
lsenaa <lsenaa3838@gmail.com>
2024-09-11 16:53:53 +0900

    fix: 로그인 확인 코드 수정

--- ProjectFiin/src/main/java/controller/MypageInfluencerInfo.java
+++ ProjectFiin/src/main/java/controller/MypageInfluencerInfo.java
@@ -33,7 +33,8 @@ public class MypageInfluencerInfo extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 로그인 여부 확인
-		if (request.getSession().getAttribute("influencer") == null) {
+		Influencer influencer = (Influencer)request.getSession().getAttribute("influencer");
+		if (influencer == null) {
 			response.setContentType("text/html; charset=UTF-8");
 			PrintWriter out = response.getWriter();
 			out.println("<script>alert('로그인이 필요한 페이지입니다'); location.href='login'; </script>"); 
