9683557a92b5d6ff90a7fb33ebcb8cad9e6890b5
SantinoMin <santinomin0381@gmail.com>
2024-09-12 11:16:29 +0900

    fix : .gitignore 추가

--- ProjectFiin/src/main/java/controller/InfluencerRegister.java
+++ ProjectFiin/src/main/java/controller/InfluencerRegister.java
@@ -8,11 +8,11 @@ import dto.Influencer;
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
 import service.InfluencerService;
 import service.InfluencerServiceImpl;
 
