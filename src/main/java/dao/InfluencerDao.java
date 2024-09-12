3c4b67f49f90b12c3e4b589e67680948577d253f
SantinoMin <santinomin0381@gmail.com>
2024-09-12 22:38:18 +0900

    fix : git updated.

--- src/main/java/dao/InfluencerDao.java
+++ src/main/java/dao/InfluencerDao.java
@@ -15,11 +15,7 @@ public interface InfluencerDao {
 	void updateInfluencer(Influencer influencer) throws Exception;
 	Integer selectAllInfluencerCount(String keyword, List<String> channels, Integer categoryId) throws Exception;
 	List<Map<String,Object>> selectAllInfluencer(Integer row, String keyword, List<String> channels, Integer categoryId) throws Exception;
-
-  List<Map<String, Object>> selectInfluencerList(Integer row, String keyword,
-      List<String> channels, Integer categoryId) throws Exception;
-
-  Integer selectInfluencerCount() throws Exception;
+	Integer selectInfluencerCount() throws Exception;
 	List<Influencer> selectInfluencerListForMain() throws Exception;
 	Influencer selectInfluencerForFindEmail(String name, String mobileNumber) throws Exception;
 	String selectInfluencerForFindPassword(String userEmail) throws Exception;
