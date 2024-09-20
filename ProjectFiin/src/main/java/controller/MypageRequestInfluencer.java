a5ec6bbdff933839d7e99a4911947922785f1785
97kmj <160502250+97kmj@users.noreply.github.com>
2024-09-20 13:53:42 +0900

    Update and rename MypageRequestedInfluencer.java to MypageRequestInfluencer.java

--- ProjectFiin/src/main/java/controller/MypageRequestInfluencer.java
+++ ProjectFiin/src/main/java/controller/MypageRequestInfluencer.java
@@ -14,7 +14,7 @@ import service.CampaignServiceImpl;
 import service.RequestCampaignService;
 import service.RequestCampaignServiceImpl;
 
-@WebServlet("/requestedInfluencer")
+@WebServlet("/requestInfluencer")
 public class mypageRequestedInfluencer extends HttpServlet {
 
   private static final long serialVersionUID = 1L;
